4e9b30404832456d17159efdeea9c4c76631490a
Caerroff <masin49@gmail.com>
2024-11-26 23:20:12 +0200

    remove _deps

--- _deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/KeySymToKeyMapping.cpp.o.d
+++
@@ -1,9 +0,0 @@
-_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/KeySymToKeyMapping.cpp.o: \
- /home/caerroff/GitHub/aquarius/_deps/sfml-src/src/SFML/Window/Unix/KeySymToKeyMapping.cpp \
- /usr/include/stdc-predef.h \
- /home/caerroff/GitHub/aquarius/_deps/sfml-src/src/SFML/Window/Unix/KeySymToKeyMapping.hpp \
- /home/caerroff/GitHub/aquarius/_deps/sfml-src/include/SFML/Window/Keyboard.hpp \
- /home/caerroff/GitHub/aquarius/_deps/sfml-src/include/SFML/Window/Export.hpp \
- /home/caerroff/GitHub/aquarius/_deps/sfml-src/include/SFML/Config.hpp \
- /usr/include/X11/X.h /usr/include/X11/keysym.h \
- /usr/include/X11/keysymdef.h
