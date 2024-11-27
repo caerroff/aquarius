222aedea15afa7786f234aabe11d69440d7213fc
Caerroff <masin49@gmail.com>
2024-11-27 14:02:05 +0200

    feat(yaml): added yaml parser compatibility for SceneManager

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -2,170 +2,191 @@
 
 Scene::Scene()
 {
-    this->name = "";
-    this->musicPath = "";
+  this->name = "";
+  this->musicPath = "";
 }
 
-Scene::Scene(char *name, char *musicPath, int numberOfFrames)
+Scene::Scene(std::string name, std::string musicPath, int numberOfFrames)
 {
-    this->name = name;
-    this->musicPath = musicPath;
-    if (musicPath)
-    {
-        this->playMusic();
-    }
-    this->numberOfFrames = numberOfFrames;
+  this->name = name;
+  this->musicPath = DEFAULT_MUSIC_PATH + musicPath;
+  if (musicPath.length())
+  {
+    this->playMusic();
+  }
+  this->numberOfFrames = numberOfFrames;
 }
 
 void Scene::playMusic()
 {
-    this->music.openFromFile(this->musicPath);
+  if (this->music.openFromFile(this->musicPath))
+  {
     this->music.setVolume(40);
     this->music.play();
+  }
+  else
+  {
+    std::cerr << "(Scene.cpp) Error while playing Music" << std::endl;
+  }
 }
 
-void Scene::setMusicPath(char *_musicPath)
+void Scene::setMusicPath(std::string _musicPath)
 {
-    this->musicPath = _musicPath;
+  this->musicPath = DEFAULT_MUSIC_PATH + _musicPath;
 }
 
 void Scene::stopMusic()
 {
-    this->music.stop();
-    while (this->music.getStatus() != sf::SoundSource::Stopped)
-    {
-        ; // Wait for music to stop
-    }
+  this->music.stop();
+  while (this->music.getStatus() != sf::SoundSource::Stopped)
+  {
+    ; // Wait for music to stop
+  }
 }
 
-void Scene::addText(const char *_string, int x, int y, int size, const char *fontPath = "assets/font/PressStart2P-Regular.ttf")
+void Scene::addText(std::string _string, int x, int y, int size, std::string fontPath = "assets/font/PressStart2P-Regular.ttf")
 {
-    sf::Text *text = new sf::Text();
-    sf::Font font;
-    font.loadFromFile(fontPath);
-    text->setFont(font);
-    text->setString(_string);
-    text->setCharacterSize(size);
-    text->setFillColor(sf::Color::White);
-    text->setOutlineColor(sf::Color::Black);
-    text->setOutlineThickness(2);
-    text->setStyle(sf::Text::Bold);
-    text->setPosition(x, y);
-    sceneTexts.push_back(text);
+  sf::Text *text = new sf::Text();
+  sf::Font *font = new sf::Font();
+  if (font->loadFromFile(fontPath))
+  {
+    text->setFont(*font);
+  }
+  text->setString(_string);
+  text->setCharacterSize(size);
+  text->setFillColor(sf::Color::White);
+  text->setOutlineColor(sf::Color::Black);
+  text->setOutlineThickness(2);
+  text->setPosition(x, y);
+
+  this->sceneTexts.push_back(text);
 }
 
-void Scene::addTextCenter(sf::RenderWindow *window, const char *_string, int size, int r = 0, int g = 0, int b = 0, const char *fontPath = "assets/font/PressStart2P-Regular.ttf")
+void Scene::addTextCenter(sf::RenderWindow *window, std::string _string, int size, int r = 0, int g = 0, int b = 0, std::string fontPath = "assets/font/PressStart2P-Regular.ttf")
 {
-    sf::Text *text = new sf::Text();
-    sf::Font *font = new sf::Font();
-    if (font->loadFromFile(fontPath))
-    {
-        text->setFont(*font);
-    }
-    else
-    {
-        fprintf(stderr, "Font at %s couldn't be loaded\n", fontPath);
-    }
-    text->setString(_string);
-    text->setCharacterSize(size);
-    text->setFillColor(sf::Color(r, g, b));
-    text->setOutlineColor(sf::Color::Black);
-    text->setOutlineThickness(1);
-    text->setPosition(window->getSize().x / 2 - text->getGlobalBounds().width / 2, window->getSize().y / 2 - text->getGlobalBounds().height / 2);
+  sf::Text *text = new sf::Text();
+  sf::Font *font = new sf::Font();
+  if (font->loadFromFile(fontPath))
+  {
+    text->setFont(*font);
+  }
+  else
+  {
+    fprintf(stderr, "Font at %s couldn't be loaded\n", fontPath);
+  }
+  text->setString(_string);
+  text->setCharacterSize(size);
+  text->setFillColor(sf::Color(r, g, b));
+  text->setOutlineColor(sf::Color::Black);
+  text->setOutlineThickness(1);
+  text->setPosition(window->getSize().x / 2 - text->getGlobalBounds().width / 2, window->getSize().y / 2 - text->getGlobalBounds().height / 2);
 
-    this->sceneTexts.push_back(text);
+  this->sceneTexts.push_back(text);
+}
+
+void Scene::addTextCenterWithY(sf::RenderWindow *window, std::string _string, int size, int y, int r = 0, int g = 0, int b = 0, std::string fontPath = "assets/font/PressStart2P-Regular.ttf")
+{
+  sf::Text *text = new sf::Text();
+  sf::Font *font = new sf::Font();
+  if (font->loadFromFile(fontPath))
+  {
+    text->setFont(*font);
+  }
+  else
+  {
+    fprintf(stderr, "Font at %s couldn't be loaded\n", fontPath);
+  }
+  text->setString(_string);
+  text->setCharacterSize(size);
+  text->setFillColor(sf::Color(r, g, b));
+  text->setOutlineColor(sf::Color::Black);
+  text->setOutlineThickness(1);
+  text->setPosition(window->getSize().x / 2 - text->getGlobalBounds().width / 2, y);
+
+  this->sceneTexts.push_back(text);
 }
 
 void Scene::changeOpacityText(int position, int opacity)
 {
-    sf::Text *text = this->getTextAt(position);
-    sf::Color color = text->getFillColor();
-    if (color.a > opacity)
-    {
-        color.a -= 1;
-    }
-    else if (color.a < opacity)
-    {
-        color.a += 1;
-    }
-    text->setFillColor(color);
-    this->sceneTexts.at(position) = text;
+  sf::Text *text = this->getTextAt(position);
+  sf::Color color = text->getFillColor();
+  if (color.a > opacity)
+  {
+    color.a -= 1;
+  }
+  else if (color.a < opacity)
+  {
+    color.a += 1;
+  }
+  text->setFillColor(color);
+  this->sceneTexts.at(position) = text;
 }
 
-void Scene::loadFromFile(const char *path, sf::RenderWindow *window)
+void Scene::loadFromFile(std::string path, sf::RenderWindow *window)
 {
-    char *name;
-    std::vector<sf::Text *> texts;
-    std::vector<sf::RectangleShape *> rectangles;
-    char *musicPath;
-    int numberOfFrames;
+  std::string name;
+  sf::Color color;
+  std::vector<sf::Text *> texts;
+  std::vector<sf::RectangleShape *> rectangles;
+  std::string musicPath;
+  int numberOfFrames;
 
-    Scene *scene = new Scene();
-    std::ifstream file;
-    file.open(DEFAULT_SCENE_PATH + std::string(path));
-    if (file.is_open())
+  try
+  {
+    YAML::Node object = YAML::LoadFile(DEFAULT_SCENE_PATH + path);
+    musicPath = object["Music"].as<std::string>();
+    this->setMusicPath(musicPath);
+
+    if (object["Background"].IsSequence() && object["Background"].size() == 3)
     {
-        std::string line;
-        while (std::getline(file, line))
+      std::vector<int> colorMap = object["Background"].as<std::vector<int>>();
+      bgColor = sf::Color(colorMap.at(0), colorMap.at(1), colorMap.at(2));
+    }
+
+    name = object["Name"].as<std::string>();
+    this->setName(name);
+
+    for (int8_t i = 0; i < object["Texts"].size(); i++)
+    {
+      if (object["Texts"][i]["Centered"].IsDefined() && object["Texts"][i]["Centered"].as<bool>())
+      {
+        std::string text = object["Texts"][i]["Text"].as<std::string>();
+        int size = object["Texts"][i]["Size"].as<int>();
+        int r = object["Texts"][i]["Red"].as<int>();
+        int g = object["Texts"][i]["Green"].as<int>();
+        int b = object["Texts"][i]["Blue"].as<int>();
+        if (!object["Texts"][i]["YCoords"].IsDefined())
         {
-            std::string delimiter = "|";
-            std::string token = line.substr(0, line.find(delimiter));
-            if (token == "Scene")
-            {
-                name = (char *)line.substr(line.find(delimiter) + 1, line.length()).c_str();
-            }
-
-            if (token == "Action")
-            {
-                ;
-            }
-
-            if (token == "Music")
-            {
-                std::string musicPath = DEFAULT_MUSIC_PATH + line.substr(line.find(delimiter) + 1, line.length());
-                char *path = (char *)calloc(musicPath.length(), sizeof(char));
-                for (int i = 0; i < musicPath.length(); i++)
-                {
-                    path[i] = musicPath[i];
-                }
-                this->setMusicPath(path);
-            }
-
-            if (token == "Text")
-            {
-                // What's to retrieve in order:
-                //  - Text
-                //  - R
-                //  - G
-                //  - B
-                //  - Is it centered
-                //  - Position X
-                //  - Position Y
-                //  They are all separated by a | character
-                std::string text = line.substr(line.find(delimiter) + 1, line.length());
-                std::string textString = text.substr(0, text.find(delimiter));
-
-                text = text.substr(text.find(delimiter) + 1, text.length());
-                int r = std::stoi(text.substr(0, text.find(delimiter)));
-                text = text.substr(text.find(delimiter) + 1, text.length());
-                int g = std::stoi(text.substr(0, text.find(delimiter)));
-                text = text.substr(text.find(delimiter) + 1, text.length());
-                int b = std::stoi(text.substr(0, text.find(delimiter)));
-                text = text.substr(text.find(delimiter) + 1, text.length());
-                std::string size = text.substr(0, text.find(delimiter));
-                text = text.substr(text.find(delimiter) + 1, text.length());
-                std::string isCentered = text.substr(0, text.find(delimiter));
-                text = text.substr(text.find(delimiter) + 1, text.length());
-                if (isCentered != "1")
-                {
-                    sf::Vector2f position = sf::Vector2f(std::stoi(text.substr(0, text.find(delimiter))), std::stoi(text.substr(text.find(delimiter) + 1, text.length())));
-                }
-                this->addTextCenter(window, (char *)textString.c_str(), std::stoi(size), r, g, b);
-            }
+          this->addTextCenter(window, text, size, r, g, b);
         }
+        else
+        {
+          int y = object["Texts"][i]["YCoords"].as<int>();
+          this->addTextCenterWithY(window, text, size, y, r, g, b);
+        }
+      }
+      else
+      {
+        std::string text = object["Texts"][i]["Text"].as<std::string>();
+        int size = object["Texts"][i]["Size"].as<int>();
+        int r = object["Texts"][i]["Red"].as<int>();
+        int g = object["Texts"][i]["Green"].as<int>();
+        int b = object["Texts"][i]["Blue"].as<int>();
+        int x = object["Texts"][i]["XCoords"].as<int>();
+        int y = object["Texts"][i]["YCoords"].as<int>();
+        this->addText(text, x, y, size);
+      }
     }
-    else
-    {
-        fprintf(stderr, "Couldn't open file %s ! \n", path);
-    }
+  }
+  catch (YAML::BadFile e)
+  {
+    std::cerr << "Error while loading Scene: " << e.msg << std::endl;
+    std::cerr << "Line: " << e.mark.line << ":" << e.mark.column << std::endl;
+    return;
+  }
+  catch (YAML::Exception e)
+  {
+    std::cerr << "Error: " << e.msg << std::endl;
+    std::cerr << e.what() << std::endl;
+  }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <SFML/Audio.hpp>
 #include "Collidables/Player.cpp"
 #include "Save.cpp"
-#include "yaml-cpp/yaml.h"
+#include <yaml-cpp/yaml.h>
 #include "Managers/SceneManager.cpp"
 #include "Managers/GameplayManager.cpp"
 
@@ -13,38 +13,16 @@ void handleKeys(sf::Keyboard::Key key, sf::RenderWindow *window);
 
 int main(void)
 {
-    try
-    {
-        YAML::Node node = YAML::LoadFile("assets/scene/opening.yaml");
-        std::cout << node["Name"] << std::endl;
-        std::cout << node["Texts"][0] << std::endl;
-    }
-    catch (YAML::BadFile e)
-    {
-        std::cerr << "Error: " << e.msg << std::endl;
-    }
     sf::RenderWindow *window = new sf::RenderWindow(sf::VideoMode(1280, 720), "Aquarius", sf::Style::Titlebar | sf::Style::Close);
     window->setFramerateLimit(60);
     window->setVerticalSyncEnabled(true);
 
     SceneManager::getSceneManager().setModeScene();
-    SceneManager::getSceneManager().loadScene("opening.scene", window);
+    SceneManager::getSceneManager().loadScene("opening.yaml", window);
     while (window->isOpen())
     {
         sf::Event e;
-        while (window->pollEvent(e))
-        {
-            if (e.type == sf::Event::Closed)
-            {
-                SceneManager::getSceneManager().stopModeScene();
-                window->close();
-                break;
-            }
-            if (e.type == sf::Event::KeyPressed)
-            {
-                handleKeys(e.key.code, window);
-            }
-        }
+        
         window->clear(sf::Color::Black);
         if (SceneManager::getSceneManager().getCurrentMode() == SCENE_CODE)
         {
@@ -56,12 +34,25 @@ int main(void)
         }
         else
         {
-            // FIX: Error when arriving here -> segmentation fault
             fprintf(stderr, "Warning ! Thrown to GameplayMode because no mode was set\n");
             GameplayManager::getGameplayManager().setModeGameplay();
             GameplayManager::getGameplayManager().update(window);
         }
         window->display();
+        while (window->pollEvent(e))
+        {
+            if (e.type == sf::Event::Closed)
+            {
+                SceneManager::getSceneManager().stopModeScene();
+                GameplayManager::getGameplayManager().stopModeGameplay();
+                window->close();
+                break;
+            }
+            if (e.type == sf::Event::KeyPressed)
+            {
+                handleKeys(e.key.code, window);
+            }
+        }
     }
     return 0;
 }
