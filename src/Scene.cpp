4142091c5ded3cf647605da2cfce290a7a8b437d
Caerroff <masin49@gmail.com>
2024-11-27 14:15:08 +0200

    fix(global): repalce C fprintf with C++ std::cerr

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -94,7 +94,7 @@ void Scene::addTextCenterWithY(sf::RenderWindow *window, std::string _string, in
   }
   else
   {
-    fprintf(stderr, "Font at %s couldn't be loaded\n", fontPath);
+    std::cerr << "Font at " << fontPath << " could not be loaded" << std::endl;
   }
   text->setString(_string);
   text->setCharacterSize(size);
